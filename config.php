8eb583db312be9c100db114827c6f8ba97be5e37
danny <daniel-oy-mis@hotmail.com>
2018-12-07 18:56:23 -0600

    :qw

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$host="localhost";
+$port=3306;
+$socket="";
+$user="root";
+$password="2017";
+$dbname="arduino";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+if (mysqli_get_connection_stats($con)){
+    //echo "Conexion establecida";
+}else{
+    echo"Error de conexion";
+}
+
+?>
+
---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+include("core/config.php");
+                $user = $_POST["name"];
+                $pass = $_POST["pass"];
+                $consulta = "SELECT * FROM users WHERE nombre='".$user."'&&pass='".$pass."'";
+                $ejecutar= mysqli_query($con, $consulta);
+
+                $fila =  mysqli_num_rows($ejecutar);
+                if($fila>0){echo "<script>window.open('home.php','_self')</script>";}
+                else{
+                    echo "<link rel='stylesheet' href='css/bootstrap.min.css'>";
+                echo "<div class='alert alert-danger mt-3 ml-2 mr-2'><strong>Error: Usuario no encontrado</strong><br><br><img src='img/fail.png' class='img img-thumbnail'><br><br><a href='index.html' class='alert-link'>Regresar</a></div>";}  
+?>
