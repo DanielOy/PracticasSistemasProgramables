19b6922039dd9e365f653c37f4cf37125c82c717
danny <daniel-oy-mis@hotmail.com>
2018-12-07 19:09:21 -0600

    Ultimo commit realizado

--- user.php
+++ user.php
@@ -7,7 +7,8 @@ include("core/config.php");
 
                 $fila =  mysqli_num_rows($ejecutar);
                 if($fila>0){echo "<script>window.open('home.php','_self')</script>";}
-                else{
+                else
+                {
                     echo "<link rel='stylesheet' href='css/bootstrap.min.css'>";
-                echo "<div class='alert alert-danger mt-3 ml-2 mr-2'><strong>Error: Usuario no Encontrado</strong><br><br><img src='img/fail.png' class='img img-thumbnail'><br><br><a href='index.html' class='alert-link'>Regresar</a></div>";}  
+                    echo "<div class='alert alert-danger mt-3 ml-2 mr-2'><strong>Error: Usuario no Encontrado</strong><br><br><img src='img/fail.png' class='img img-thumbnail'><br><br><a href='index.html' class='alert-link'>Regresar</a></div>";}  
 ?>
